a466f6c18e2bc866fb1c3e7dff332f9477c58dee
KayoRonald <kayoronald4321@gmail.com>
2022-05-07 23:47:01 +0000

    fix: unique

--- src/database/migrations/20220427233702_epice_db.ts
+++ src/database/migrations/20220427233702_epice_db.ts
@@ -1,16 +1,17 @@
 import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-  return await knex.schema.createTable('epice_user', (table) => {
-    table.increments('id').unique().primary();
+  return await knex.schema.createTable('database_epice', (table) => {
+    table.increments('id').primary();
     table.text('name').notNullable();
-    table.text('email').notNullable().unique().;
+    table.text('email').notNullable();
     table.text('curso').notNullable();
-    table.timestamp('created_at').defaultTo(Date.now());
-    table.timestamp('update_at').defaultTo(Date.now());
-  });
+    table.unique(['email', 'id']);
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+    table.timestamp('updated_at').defaultTo(knex.fn.now())
+  })
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return await knex.schema.dropTable('epice_user');
+  return await knex.schema.dropTable('database_epice');
 }
