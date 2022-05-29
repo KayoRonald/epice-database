7704630583fd5547b2cff9de5e5c125cd4797657
KayoRonald <kayoronald4321@gmail.com>
2022-05-29 23:57:20 +0000

    feat: create yup

--- src/modules/shared/routes/routes.ts
+++ src/modules/shared/routes/routes.ts
@@ -1,17 +1,7 @@
 import { Router } from 'express'
-import { ListServiceUserController } from '../../controllers/ListServiceUserController'
-import { CreateUserController } from '../../controllers/CreateUserController'
-import { DeleterControllerUser } from '../../controllers/DeleterControllerUser'
-import { UpdateUserControllerName } from '../../controllers/UpdateUserControllerName'
-import { UpdateUserControllerCurso } from '../../controllers/UpdateUserControllerCurso'
+import { CreateUser, DeleteUser, ListUser, UpdateCurso, UpdateName } from '../../controllers'
 
 const routes = Router()
-
-const ListUser = new ListServiceUserController()
-const CreateUser = new CreateUserController()
-const DeleteUser = new DeleterControllerUser()
-const UpdateName = new UpdateUserControllerName()
-const UpdateCurso = new UpdateUserControllerCurso()
 
 export default routes
   .get('/epice', ListUser.handle)
