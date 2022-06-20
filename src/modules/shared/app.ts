0393cd49c11a25bc7cd4fe275c0674ff31f0f68e
KayoRonald <kayoronald4321@gmail.com>
2022-06-20 11:26:44 +0000

    feat: api-docs

--- src/modules/shared/app.ts
+++ src/modules/shared/app.ts
@@ -2,14 +2,18 @@ import 'express-async-errors'
 import express, { NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import Chalk from 'chalk'
+import swaggerUi from 'swagger-ui-express'
+// import * as basicAuth from 'express-basic-auth'
 import routes from './routes/routes'
+import swaggerDocs from '../../docs/swagger.json'
 import AppError from '../../middleware/AppError'
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.use(routes)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+app.use('/v1', routes)
 
 app.use(
   (error: Error, request: Request, response: Response, _: NextFunction) => {
