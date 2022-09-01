1037c7bdb9104bcfd3b4f0ecb7aa39ec1cd38c14
Kayo Ronald <kayoronald4321@gmail.com>
2022-09-01 13:46:12 +0000

    chore: delete knexjs

--- src/connection/connection.ts
+++
@@ -1,8 +0,0 @@
-import knex from 'knex'
-import dotenv from 'dotenv'
-import knexFile from '../../knexfile'
-import config from '../config'
-
-dotenv.config()
-
-export default knex(knexFile[String(config.environment)])
