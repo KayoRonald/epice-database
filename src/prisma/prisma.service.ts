7c8d3f6bfb4f6e854bddb5119f9cd51fa9ddeadc
Kayo Ronald <kayoronald4321@gmail.com>
2022-09-04 19:14:22 +0000

    chore(teste): create middleware prisma hash

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,18 @@
 import { PrismaClient } from '@prisma/client'
+// import * as bcrypt from 'bcrypt'
 
 export const prisma = new PrismaClient()
+
+// Middleware Prisma hash
+
+prisma.$use(async (params, next) => {
+  if (params.model === 'Admin' && params.action === 'create') {
+    console.log(params.args.data.password)
+    // const hashedPassword = await bcrypt.hash(params.args.data.password, 10)
+    // params.args.data.password = hashedPassword
+  }
+  return next(params)
+})
 
 const bug = async () => {
   try {
