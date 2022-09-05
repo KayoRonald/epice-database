66bdb154f8aa69cdf5c12f1b504e178f956d8da7
Kayo Ronald <kayoronald4321@gmail.com>
2022-09-05 00:54:53 +0000

    chore: format prettier

--- src/middleware/validation/validateRequest.ts
+++ src/middleware/validation/validateRequest.ts
@@ -3,16 +3,18 @@ import { Response, NextFunction } from 'express'
 import { IUSerRequest } from '../../types'
 import { BadRequestException } from '../../common/exceptions'
 
-const validate = (schema: AnySchema) => async (req: IUSerRequest, res: Response, next: NextFunction) => {
-  try {
-    await schema.validate({
-      body: req.body,
-      params: req.params
-    })
-    next()
-  } catch (error: any) {
-    throw new BadRequestException(error.message)
-  }
-}
+const validate =
+  (schema: AnySchema) =>
+    async (req: IUSerRequest, res: Response, next: NextFunction) => {
+      try {
+        await schema.validate({
+          body: req.body,
+          params: req.params
+        })
+        next()
+      } catch (error: any) {
+        throw new BadRequestException(error.message)
+      }
+    }
 
 export default validate
